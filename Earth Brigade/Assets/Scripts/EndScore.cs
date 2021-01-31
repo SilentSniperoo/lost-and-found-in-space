93efff07e79147289f8a176d0193ef47e734f7f6
Jubal-N <jubal@heytutor.com>
2021-01-31 14:22:46 -0800

    Game over score

---
+++ Earth Brigade/Assets/Scripts/EndScore.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndScore : MonoBehaviour
+{
+    public Sprite[] digits = new Sprite[10];
+
+    public Image tensSecondsImage;
+    public Image onesSecondsImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tensSecondsImage.sprite = digits[Mathf.Clamp(ScoreStatus.Score / 10, 0, 9)];
+        onesSecondsImage.sprite = digits[Mathf.Clamp(ScoreStatus.Score % 10, 0, 9)];
+    }
+}
